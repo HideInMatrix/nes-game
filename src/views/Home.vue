36c524c96c5529192be0152da1222133f516ca47
HideInMatrix <davidmorgancreate@gmail.com>
2025-02-22 13:16:19 +0800

    fix: 原生广告删除多余的

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,7 +58,7 @@ onBeforeMount(() => {
             :ad-style="{
               display: 'inline-block',
               width: '100%',
-              height: '150px',
+              height: '100%',
               border: '1px solid #eee',
             }"
             native-format="image-centered" />
